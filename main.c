7b6af0ac464b696afe4f8ccc9f492f04e4b08b45
Amitava Mitra <mitraamitava7@gmail.com>
2017-04-03 22:48:50 +0530

    shortlog: issue fixed in line 21
    the minor bug regarding negative argument is fixed.
    
    fixes: #13
    
    signed-off by:Amitava123<mitraamitava7@gmail.com>

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
 		printf("Usage: %s n\n", argv[0]);
 		return 1;
 	}
-	if(argc<2){
+	if(argv[1]<0){
 		printf("Negative arguments are not allowed!");
 		return 1;
 	}
